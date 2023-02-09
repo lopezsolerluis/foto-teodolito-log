7e603da129bdb52579f3fa534f0b424e3fa19049
Luis G. López <lopezsolerluis@gmail.com>
2023-02-09 07:25:50 -0300

    Park telescope when finished. Some minor names changes

--- foto-teodolito-main.py
+++ foto-teodolito-main.py
@@ -1,4 +1,3 @@
-# Imports go at the top
 from microbit import *
 import log
 from pca9685 import *
@@ -9,19 +8,19 @@ log.set_labels('altitude', 'azimuth', 'measure', timestamp=log.SECONDS)
 
 servo = 1 # Servo channel
 stepper = 1 # Stepper channel
-reduction = 2 # Relation between the grears of the 'foto-teodolito'
+reduction = 2 # Relation between the gears of the 'foto-teodolito'
 pause_between_measures = 1000 # milliseconds
 min_altitude = 30
-delta_alpha = 30
+delta_altitude = 30
 measure = 0
 
 # Wait until Button 'A' is pressed. This way, we can sinchronize the measures with an external clock or watch
 while not button_a.was_pressed():
     pass
 
-for altitude in range(min_altitude, 90+delta_alpha, delta_alpha):
+for altitude in range(min_altitude, 90+delta_altitude, delta_altitude):
     pca.setServoDegrees(servo, altitude)
-    steps_per_altitude = round(360*cos(radians(altitude)) / delta_alpha)
+    steps_per_altitude = round(360*cos(radians(altitude)) / delta_altitude)
     delta_azimuth = 360 / steps_per_altitude
     azimuth = 0
     while azimuth < 360:
@@ -34,3 +33,5 @@ for altitude in range(min_altitude, 90+delta_alpha, delta_alpha):
         })
         pca.moveStepperDegreesBlocking(stepper, delta_azimuth*reduction)
         azimuth += delta_azimuth
+
+pca.setServoDegrees(servo, 0) # Parking position
