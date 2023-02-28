793631d6dad887e73d3ecf1156d6230bd70137c4
Luis G. López <lopezsolerluis@gmail.com>
2023-02-28 15:18:00 -0300

    Added remote control (kind of demo)

--- colors.py
+++ colors.py
@@ -1,2 +1,3 @@
 busy = (200,0,0)
 available = (0,200,0)
+remote_available = (0,0,150)
--- foto-teodolito-main.py
+++ foto-teodolito-main.py
@@ -5,6 +5,8 @@ from bh1750 import *
 from ldr import *
 from math import cos, radians
 import neodisplay
+import colors
+import radio
 
 servo = 1 # Servo channel
 stepper = 1 # Stepper channel
@@ -14,8 +16,16 @@ min_altitude = 20 # We don't want to measure the buildings, right? :D
 delta_altitude = 10
 
 pca = PCA9685()
+radio.off()
 
 first_reading = True
+
+def show_available():
+    neodisplay.show_available(0)
+
+def show_remote_control_available():
+    neodisplay.show_state(3,colors.remote_available)
+
     
 def take_readings(sensor):
     global first_reading
@@ -42,12 +52,40 @@ def take_readings(sensor):
             azimuth += delta_azimuth
     pca.setServoDegrees(servo, 0) # Parking position
     sensor.stop()
-    neodisplay.show_available(0)
+    show_available()
 
-neodisplay.show_available(0)
+
+def remote_control_mode():
+    show_remote_control_available()
+    sleep(500) # For not exiting upon arriving... :p
+    radio.on()
+    altitude = 0
+    vel_azimuth = 0
+    while not pin_logo.is_touched():
+        msg = radio.receive()
+        if msg is not None:
+            altitude = int(msg[0:3])
+            vel_azimuth = int(msg[3:6])
+        
+            pca.setServoDegrees(1,altitude)
+            if vel_azimuth > 10:
+                pca.startStepper(1, False)
+            elif vel_azimuth < -10:
+                pca.startStepper(1, True)
+            else:
+                pca.stopStepper(1)
+        sleep(50)
+    radio.off() 
+    show_available()
+    sleep(500) # Same as before
+  
+    
+show_available()
 
 while True:
     if button_a.is_pressed():
         take_readings(LDR(ldr_pin))
     elif button_b.is_pressed():
-        take_readings(BH1750()) 
+        take_readings(BH1750())
+    elif pin_logo.is_touched():
+        remote_control_mode()
